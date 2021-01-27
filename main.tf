1a72a06af35bd7c0a91b7a4de4465c8c0ded4a1a
Mike <Mike@boomking.co.uk>
2021-01-27 16:49:57 +0000

    3

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      # Specify what version of the provider we are going to utilise
-      source = "hashicorp/azurerm"
-      version = ">= 2.4.1"
-    }
-  }
-}
 provider "azurerm" {
   features {
       key_vault {
